80ac041a3c0411a2952e6a50646c8fdc81f30d64
Renie <renie.siqueira@gmail.com>
2011-07-01 03:22:09 -0300

    Second effect has been created and logic improved

--- slideshow.js
+++ slideshow.js
@@ -14,25 +14,152 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-(function($){  
-	$.fn.slideshow = function(options) {  
+
+(function(jQuery){  
+	jQuery.fn.slideshow = function(options) {  
 		
-		var itemWidth = this.find('ul li').first().width();
-		var widthSlider = this.find('ul li').length * itemWidth;
+		/*Plugin attributes*/
+		var _element;
+		var _itemCollectionParent;
+		var _itemCollection;
+		var _itemCollectionLength;
+		var _timeCounter;
+		var _currentItem;
+		var _firstItem;
+		var _lastItem;
+		var _tmpFn;
+		var _target;
+		var _effects;
 		
-		this.find('ul').css('width', widthSlider);
-		this.find('ul li').delegate('a', 'click', function(){
-			if($(this).parent().is(':last-child')){
-				$(this).parent().parent().animate({
-													marginLeft: 0
-												  }, 1500 );
+		/*Call a 'fake constructor' */
+		init(this);
+
+		/*Load default or given params */
+		var defaults = {
+			itemWidth: _firstItem.find('img').width(),
+			itemHeight: _firstItem.find('img').height(),
+			widthSlider: _itemCollection.length * _firstItem.find('img').width(),
+			transitionSpeed: 1500,
+			pauseTime: 3000,
+			effect: 'fade'
+		}
+
+		var _this = jQuery.extend(defaults, options);
+		
+		/*Prepare elements to an specific effect */
+		prepareEffect();
+		
+		/*Start effect*/
+		start();
+		
+		/**
+		 *This function sets some values to plugin attributes e CSS properties
+		 **/
+		function init(el){
+			_element = el;
+			_element.css({visibility: 'hidden', overflow: 'hidden'});
+			_itemCollectionParent	= _element.find('ul')
+			_itemCollection 		= _itemCollectionParent.find('li');
+			_itemCollectionLength	= _itemCollection.length - 1;
+			_firstItem				= _itemCollection.first();
+			_lastItem				= _itemCollection.last();
+			_effects 				= {horizontalSlide: hSlide, fade: fade};
+			_prepareEffects 		= {horizontalSlide: hSlidePrepare, fade: fadePrepare};
+				
+		}
+		
+		/**
+		 *Functions that sets some specific CSS properties to each effect
+		 **/
+		function prepareEffect(){
+			_itemCollectionParent.css({width: _this.widthSlider, height: _this.itemHeight, display: 'block'});
+			_element.css({width: _this.itemWidth, height: _this.itemHeight});
+			_prepareEffects[_this.effect]();	
+		}
+		
+		function basicUnitPrepare(el){
+			jQuery(el).css({
+					listStyle: 'none',
+					display: 'block',
+					width: _this.itemWidth,
+					height: _this.itemHeight
+				});
+		}
+		
+		function fadePrepare(){
+			cont = 0;
+			_itemCollection.each(function(){
+				basicUnitPrepare(this);
+				jQuery(this).css({
+					position: 'absolute'
+				});
+				if(cont!=0)
+					jQuery(this).css({ opacity: 0, zIndex: -9999 });
+				else
+					jQuery(this).css({ zIndex: 0 });
+				cont--;
+			});			
+		}
+		
+		function hSlidePrepare(){
+			_itemCollection.each(function(){
+				jQuery(this).css({
+					float: 'left'
+				});
+			})
+		}
+		
+		/**
+		 *This function starts an auto plugin animation
+		 **/
+		function start(){
+			_currentItem = _firstItem;
+			_tmpFn = _effects[_this.effect];
+			_element.css({visibility: 'visible'})
+			_timeCounter = setInterval(logicalSequenceAnimation,_this.pauseTime);		
+		}
+		
+		/**
+		 *This function stops the animation
+		 **/
+		function stop(){
+			clearInterval(_timeCounter);		
+		}
+		
+		/**
+		 *Here is the animation logic
+		 **/
+		function logicalSequenceAnimation(){
+			if( _currentItem.get(0) == _lastItem.get(0) ){				
+				_tmpFn(true)
+				_currentItem = _firstItem;
 			}else{
-				$(this).parent().parent().animate({
-													marginLeft: "-="+itemWidth
-												  }, 1500 );
+				_tmpFn(false);
+				_currentItem = _currentItem.next();
 			}
-			return false;	
-		});
+		}
 		
+		/**
+		 *Horizontal slide animation
+		 **/
+		function hSlide(last){
+			target = _currentItem.parent();
+			if(last)
+				target.animate({marginLeft: 0}, _this.transitionSpeed );
+			else
+				target.animate({marginLeft: "-="+_this.itemWidth}, _this.transitionSpeed );
+		}
+		
+		/**
+		 *Fade animation
+		 **/
+		function fade(last){
+			_currentItem.animate({opacity: 0, zIndex: -9999 }, _this.transitionSpeed / 2 );
+			if(last)	
+				_firstItem.animate({opacity: 1, zIndex: 0 }, _this.transitionSpeed / 2 );
+			else
+				_currentItem.next().animate({opacity: 1, zIndex: 0 }, _this.transitionSpeed / 2 );
+			
+		}
 	};  
 })(jQuery);
